3a7403c4a89824833131b370ec723d12dd8a2d60
KavyaRamasamy16 <136969293+KavyaRamasamy16@users.noreply.github.com>
2024-07-08 10:06:17 +0530

    Add files via upload

---
+++ 02_JS_variables/Ch2_pgm_03.js
@@ -0,0 +1,12 @@
+var score;
+score = 100;
+console.log(score);
+
+// Changing the value for variable
+score = 88;
+console.log(score);
+
+// Declaring the second variable
+var score2;
+score2 = 77;
+console.log(score2);
---
+++ 02_JS_variables/Ch2_pgm_04.js
@@ -0,0 +1,15 @@
+var score;
+score = 100;
+console.log(score);
+
+score = 130;
+console.log(score);
+
+// Declaring second variable
+var score2;
+score2 = 90;
+console.log(score2);
+
+// Altering the second variable
+score2 = 85;
+console.log(score2);
